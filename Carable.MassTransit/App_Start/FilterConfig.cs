ee80a3395cef858d966884c6f98fda7e3892f82c
Oskar Gewalli <oskar.gewalli@garantibil.se>
2016-10-17 15:48:22 +0200

    Add project files.

---
+++ Carable.MassTransit/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Carable.MassTransit
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Carable.MassTransit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Carable.MassTransit.MvcApplication" Language="C#" %>
